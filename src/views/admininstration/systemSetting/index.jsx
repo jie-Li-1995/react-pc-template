e15119cc8f1075872b2df7b1eaa86e5e23dabc0a
“jie-Li-1995” <2893535402@qq.com>
2020-01-13 11:40:27 +0800

    prop-types

--- src/views/admininstration/systemSetting/index.jsx
+++ src/views/admininstration/systemSetting/index.jsx
@@ -1,10 +1,19 @@
 import React, {Component} from 'react'
+import propTypes from 'prop-types'
 
 export default class SystemSetting extends Component {
+    static propTypes = {
+        name: propTypes.string.isRequired
+    }
+    static defaultProps = {
+        name: '刘淇浩'
+    }
     render() {
        return (
            <div className='SystemSetting'>
                SystemSetting
+               <br/>
+               {this.props.name}
            </div>
        )
     }
